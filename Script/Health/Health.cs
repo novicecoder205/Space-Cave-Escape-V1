38f37d6d816b12934ffabbd5651a434693f324e9
novicecoder205 <warpick2000@gmail.com>
2024-08-11 17:01:15 +0700

    project start

---
+++ Script/Health/Health.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System.Collections;
+//look careful, one steps will fuck this entire thing up!
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+    public void TakeDamage(float _damage)
+    {
+        if (invulnerable) return;
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+
+                //Player
+                //if (GetComponent<PlayerMovement>() != null)
+                    //GetComponent<PlayerMovement>().enabled = false;
+
+                //Enemy
+                //if (GetComponent<EnemyPatrol>() != null)
+                    //GetComponentInParent<EnemyPatrol>().enabled = false;
+
+                //MeleeEnemy
+                //if (GetComponent<MeleeEnemy>() != null)
+                    //GetComponent<MeleeEnemy>().enabled = false;
+
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
+
+                dead = true;
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    private IEnumerator Invunerability()
+    {
+        invulnerable = true;
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
